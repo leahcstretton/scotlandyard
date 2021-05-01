6bf916314a98e04f03f18b1b5aefdd0b870c4cd5
Andy Stretton <strets123@gmail.com>
2021-05-01 09:16:09 +0100

    Initial

---
+++ scotlandyard.py
@@ -0,0 +1,905 @@
+
+class BasePlace:
+    """The base place class"""
+
+x=0
+
+class Place1(BasePlace):
+
+    bus_to = [2,3]
+ 
+    taxi_to = [3]
+    train_to = []
+    tram_to = []
+
+class Place2(BasePlace):
+    number = 2
+    bus_to = [1]
+    taxi_to = [59]
+    train_to = [30]
+    tram_to = []
+
+class Place3(BasePlace):
+    number = 3
+    bus_to = [1,4]
+    taxi_to = [1,4,59]
+    train_to = []
+    tram_to = []
+
+#no place 5
+
+class Place4(BasePlace):
+    number = 4
+    bus_to = [3,17,15,27]
+    taxi_to = [3,6,25,59]
+    train_to = [26,18]
+    tram_to = []
+
+class Place5(BasePlace):
+    number = 5
+    bus_to = []
+    taxi_to = [137]
+    train_to = [30]
+    tram_to = []
+
+
+class Place6(BasePlace):
+    number = 6
+    bus_to = []
+    taxi_to = [4,25,13]
+    train_to = []
+    tram_to = []
+
+class Place7(BasePlace):
+    number = 7
+    bus_to = []
+    taxi_to = [9,8,4]
+    train_to = []
+    tram_to = []
+
+class Place8(BasePlace):
+    number = 8
+    bus_to = []
+    taxi_to = [27,7,9]
+    train_to = []
+    tram_to = []
+
+class Place9(BasePlace):
+    number = 9
+    bus_to = []
+    taxi_to = [7,8]
+    train_to = []
+    tram_to = []
+
+class Place10(BasePlace):
+    number = 10
+    bus_to = [30]
+    taxi_to = [11]
+    train_to = []
+    tram_to = []
+
+class Place11(BasePlace):
+    number = 11
+    bus_to = []
+    taxi_to = [10,30]
+    train_to = []
+    tram_to = []
+
+class Place12(BasePlace):
+    number = 12
+    bus_to = []
+    taxi_to = [11,30]
+    train_to = []
+    tram_to = []
+
+class Place13(BasePlace):
+    number = 13
+    bus_to = []
+    taxi_to = [6]
+    train_to = []
+    tram_to = []
+
+class Place14(BasePlace):
+    number = 14
+    bus_to = []
+    taxi_to = [15,16]
+    train_to = []
+    tram_to = []
+
+class Place15(BasePlace):
+    number = 15
+    bus_to = [4]
+    taxi_to = [27,24]
+    train_to = []
+    tram_to = []
+
+class Place16(BasePlace):
+    number = 16
+    bus_to = [17,14]
+    taxi_to = [14]
+    train_to = []
+    tram_to = []
+
+class Place17(BasePlace):
+    number = 17
+    bus_to = [4,16,18]
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class Place18(BasePlace):
+    number = 18
+    bus_to = [17,19]
+    taxi_to = [13]
+    train_to = [4]
+    tram_to = []
+
+class Place19(BasePlace):
+    number = 19
+    bus_to = [21,18]
+    taxi_to = [20]
+    train_to = []
+    tram_to = []
+
+class Place20(BasePlace):
+    number = 20
+    bus_to = [23,21]
+    taxi_to = [22,19]
+    train_to = []
+    tram_to = []
+
+class Place21(BasePlace):
+    number = 21
+    bus_to = [19,20]
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class Place22(BasePlace):
+    number = 22
+    bus_to = []
+    taxi_to = [20,23]
+    train_to = []
+    tram_to = []
+
+class Place23(BasePlace):
+    number = 23
+    bus_to = [16,20,66,55,20]
+    taxi_to = [22,24,55]
+    train_to = []
+    tram_to = []
+
+class Place24(BasePlace):
+    number = 24
+    bus_to = []
+    taxi_to = [15,16,23]
+    train_to = []
+    tram_to = []
+
+class Place25(BasePlace):
+    number = 25
+    bus_to = []
+    taxi_to = [4,6]
+    train_to = []
+    tram_to = []
+
+class Place26(BasePlace):
+    number = 26
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class Place27(BasePlace):
+    number = 27
+    bus_to = [4]
+    taxi_to = [28,8]
+    train_to = [26]
+    tram_to = []
+
+class Place28(BasePlace):
+    number = 28
+    bus_to = []
+    taxi_to = [29,30,27]
+    train_to = []
+    tram_to = []
+
+class Place29(BasePlace):
+    number = 29
+    bus_to = []
+    taxi_to = [28,30]
+    train_to = []
+    tram_to = [32]
+
+class Place30(BasePlace):
+    number =30
+    bus_to = []
+    taxi_to = [28,29,31]
+    train_to = [2,5,51]
+    tram_to = []
+
+class Place31(BasePlace):
+    number = 31
+    bus_to = []
+    taxi_to = [30,29]
+    train_to = []
+    tram_to = []
+
+class Place32(BasePlace):
+    number = 32
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = [29,50]
+
+class Place33(BasePlace):
+    number = 33
+    bus_to = []
+    taxi_to = [34,137]
+    train_to = []
+    tram_to = []
+
+class Place34(BasePlace):
+    number = 34
+    bus_to = []
+    taxi_to = [35,33]
+    train_to = [39,51]
+    tram_to = []
+
+class Place35(BasePlace):
+    number = 35
+    bus_to = []
+    taxi_to = [34,36,137]
+    train_to = []
+    tram_to = []
+
+class Place36(BasePlace):
+    number = 36
+    bus_to = []
+    taxi_to = [35,37]
+    train_to = []
+    tram_to = []
+
+class Place37(BasePlace):
+    number = 37
+    bus_to = []
+    taxi_to = [36,38,49]
+    train_to = []
+    tram_to = []
+
+class Place38(BasePlace):
+    number = 38
+    bus_to = []
+    taxi_to = [37,39,40]
+    train_to = []
+    tram_to = []
+
+class Place39 (BasePlace):
+    number = 39
+    bus_to = []
+    taxi_to = [38]
+    train_to = [34,97]
+    tram_to = []
+
+class Place40(BasePlace):
+    number = 40
+    bus_to = []
+    taxi_to = [38,41]
+    train_to = []
+    tram_to = []
+
+class Place41(BasePlace):
+    number = 41
+    bus_to = []
+    taxi_to = [42,40,136]
+    train_to = []
+    tram_to = []
+
+class Place42(BasePlace):
+    number = 42
+    bus_to = []
+    taxi_to = [41,43,44,134]
+    train_to = []
+    tram_to = []
+
+class Place43(BasePlace):
+    number = 43
+    bus_to = [132,46,95]
+    taxi_to = [42,45,93,95]
+    train_to = []
+    tram_to = []
+
+class Place44(BasePlace):
+    number = 44
+    bus_to = []
+    taxi_to = [42,45,47]
+    train_to = []
+    tram_to = []
+
+class Place45(BasePlace):
+    number = 45
+    bus_to = []
+    taxi_to = [43,44]
+    train_to = []
+    tram_to = []
+
+class Place46(BasePlace):
+    number = 46
+    bus_to = [43]
+    taxi_to = [47,94]
+    train_to = []
+    tram_to = []
+
+class Place47(BasePlace):
+    number = 47
+    bus_to = []
+    taxi_to = [44,48]
+    train_to = []
+    tram_to = []
+
+class Place48(BasePlace):
+    number = 48
+    bus_to = []
+    taxi_to = [47,49,37]
+    train_to = []
+    tram_to = []
+
+class Place49(BasePlace):
+    number = 49
+    bus_to = [87]
+    taxi_to = [48]
+    train_to = []
+    tram_to = []
+
+class Place50(BasePlace):
+    number = 50
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = [52,32]
+
+class Place51(BasePlace):
+    number = 51
+    bus_to = []
+    taxi_to = []
+    train_to = [52,34]
+    tram_to = []
+
+class Place52(BasePlace):
+    number = 52
+    bus_to = [54,87,85,30]
+    taxi_to = []
+    train_to = [53,51,85]
+    tram_to = [87,50]
+
+class Place53(BasePlace):
+    number = 53
+    bus_to = []
+    taxi_to = [54]
+    train_to = [52,27]
+    tram_to = []
+
+class Place54(BasePlace):
+    number = 54
+    bus_to = [52,27,139]
+    taxi_to = [53,63,139]
+    train_to = []
+    tram_to = []
+
+class Place55(BasePlace):
+    number = 55
+    bus_to = [23,54]
+    taxi_to = [23,57,139]
+    train_to = []
+    tram_to = []
+
+class Place56(BasePlace):
+    number = 56
+    bus_to = []
+    taxi_to = [58,57,23]
+    train_to = []
+    tram_to = []
+
+class Place57(BasePlace):
+    number = 57
+    bus_to = []
+    taxi_to = [55,56,58,60,63]
+    train_to = []
+    tram_to = []
+
+class Place58(BasePlace):
+    number = 58
+    bus_to = []
+    taxi_to = [61,57,56]
+    train_to = []
+    tram_to = []
+
+class Place59(BasePlace):
+    number = 59
+    bus_to = []
+    taxi_to = [2,3,4]
+    train_to = []
+    tram_to = []
+
+class Place60(BasePlace):
+    number = 60
+    bus_to = []
+    taxi_to = [61,62,57]
+    train_to = []
+    tram_to = []
+
+class Place61(BasePlace):
+    number = 61
+    bus_to = []
+    taxi_to = [60,62,66]
+    train_to = []
+    tram_to = []
+
+class Place62(BasePlace):
+    number = 62
+    bus_to = []
+    taxi_to = [60,61,64,65]
+    train_to = []
+    tram_to = []
+
+class Place63(BasePlace):
+    number = 63
+    bus_to = []
+    taxi_to = [54,68,57,64]
+    train_to = []
+    tram_to = []
+
+class Place64(BasePlace):
+    number = 64
+    bus_to = []
+    taxi_to = [67,63,62,65]
+    train_to = []
+    tram_to = []
+
+class Place65(BasePlace):
+    number = 65
+    bus_to = []
+    taxi_to = [64,62,66]
+    train_to = []
+    tram_to = []
+
+class Plac66(BasePlace):
+    number = 66
+    bus_to = [69,23,67]
+    taxi_to = [65]
+    train_to = [69,71]
+    tram_to = []
+
+class Place67(BasePlace):
+    number = 67
+    bus_to = [85,66]
+    taxi_to = [64,68]
+    train_to = []
+    tram_to = []
+
+class Place68(BasePlace):
+    number = 68
+    bus_to = []
+    taxi_to = [67,85,63]
+    train_to = []
+    tram_to = []
+
+
+class Place69(BasePlace):
+    number = 69
+    bus_to = [66,85]
+    taxi_to = [66]
+    train_to = [65,71,85]
+    tram_to = []
+
+class Place70(BasePlace):
+    number = 70
+    bus_to = []
+    taxi_to = [71,65,66]
+    train_to = []
+    tram_to = []
+
+class Place71(BasePlace):
+    number = 71
+    bus_to = []
+    taxi_to = [72]
+    train_to = [66,69,70]
+    tram_to = []
+
+class Place72(BasePlace):
+    number = 72
+    bus_to = []
+    taxi_to = [74,75,71]
+    train_to = []
+    tram_to = []
+
+class Place73(BasePlace):
+    number = 73
+    bus_to = []
+    taxi_to = [78]
+    train_to = []
+    tram_to = []
+
+class Place74(BasePlace):
+    number = 74
+    bus_to = [76,85]
+    taxi_to = [73,72,114,75]
+    train_to = []
+    tram_to = []
+
+class Place75(BasePlace):
+    number = 75
+    bus_to = []
+    taxi_to = [72,74,76]
+    train_to = []
+    tram_to = []
+
+class Place76(BasePlace):
+    number = 76
+    bus_to = [74]
+    taxi_to = [75,77]
+    train_to = []
+    tram_to = []
+
+class Place77(BasePlace):
+    number = 77
+    bus_to = []
+    taxi_to = [114,115,76]
+    train_to = []
+    tram_to = []
+
+class Place78(BasePlace):
+    number = 78
+    bus_to = []
+    taxi_to = [79,73]
+    train_to = []
+    tram_to = []
+
+class Place79(BasePlace):
+    number = 79
+    bus_to = [80,84]
+    taxi_to = [84,78]
+    train_to = []
+    tram_to = []
+
+class Place80(BasePlace):
+    number = 80
+    bus_to = [90,85]
+    taxi_to = [86,83,81]
+    train_to = []
+    tram_to = []
+
+class Place81(BasePlace):
+    number = 81
+    bus_to = []
+    taxi_to = [133,82,80]
+    train_to = []
+    tram_to = []
+
+class Place82(BasePlace):
+    number = 82
+    bus_to = []
+    taxi_to = [81,133,83]
+    train_to = []
+    tram_to = []
+
+class Place83(BasePlace):
+    number = 83
+    bus_to = []
+    taxi_to = [82,80,84]
+    train_to = []
+    tram_to = []
+
+class Place84(BasePlace):
+    number = 84
+    bus_to = [79,114]
+    taxi_to = [79,114,83]
+    train_to = []
+    tram_to = []
+
+class Place85(BasePlace):
+    number = 85
+    bus_to = [69,65,80]
+    taxi_to = [68,52]
+    train_to = [69,86,52]
+    tram_to = []
+
+class Place86(BasePlace):
+    number = 86
+    bus_to = []
+    taxi_to = [80]
+    train_to = [81,85]
+    tram_to = [87]
+
+class Place87(BasePlace):
+    number = 87
+    bus_to = [88,52]
+    taxi_to = []
+    train_to = []
+    tram_to = [86,88,52]
+
+class Place88(BasePlace):
+    number = 88
+    bus_to = [90,87]
+    taxi_to = []
+    train_to = []
+    tram_to = [90,87]
+
+class Place89(BasePlace):
+    number = 89
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class Place90(BasePlace):
+    number = 90
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class Place91(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+class PlaceX(BasePlace):
+    number = x
+    bus_to = []
+    taxi_to = []
+    train_to = []
+    tram_to = []
+
+
+class Player:
+    def __init__(self, player_number, name, is_imposter):
+        self.player_number = player_number
+        self.name = name
+        self.is_imposter = is_imposter
+        self.taxi_count = 40
+        self.bus_count = 16
+        self.train_count = 5
+        self.tram_count = 4
+
+def get_player_count():
+    return int(raw_input("how many players are there?\n"))
+
+def get_players(player_count):
+    imposter_decided = False
+    players = []
+    for player_number in range(1, player_count +1):
+        prompt = "what is player {player_number}'s name\n".format(player_number=player_number)
+        name = raw_input(prompt)
+        is_imposter = False
+        if not imposter_decided:
+            is_imposter = raw_input("are you the imposter?\ny/n\n") == "y"
+            imposter_decided += is_imposter
+        players.append(Player(player_number, name, is_imposter))
+    return players
+
+def get_places():
+    for cls in BasePlace.__subclasses__():
+        print(cls.number)        
+
+def main():
+    get_places()    
+    player_count = get_player_count()
+    players = get_players(player_count)
+
+
+if __name__=="__main__":
+   main()
+
